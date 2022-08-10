44ff3ab6c24e42bfdfd9f176be8b5701879e2939
Glass <46128188+Jin-bao@users.noreply.github.com>
2022-08-10 12:56:23 +0800

    Delete Python directory

--- Python/P5-最小公倍数.py
+++
@@ -1,3 +0,0 @@
-from math import lcm
-
-print(lcm(*range(1,21)))
--- Python/P6-平方的和与和的平方之差.py
+++
@@ -1,9 +0,0 @@
-def difference(num):
-  squareSum = 0
-  sumSquare = 0
-  for i in range(1,num+1):
-    squareSum = squareSum + i**2
-  sumSquare = int(((1+num)*num/2))**2
-  return sumSquare-squareSum
-
-print(difference(100))
