f769b42d728af98e9709941b0dd4687eb8090d17
Glass <46128188+Jin-bao@users.noreply.github.com>
2022-08-21 13:47:09 +0800

    Add files via upload

--- Python/P014-最长考拉兹序列.py
+++ Python/P014-最长考拉兹序列.py
@@ -1,14 +1,20 @@
 import time
 
 def Collatz(num):
+  if num in CollatzDict:
+    return CollatzDict[num]
   if num == 1:
-    return 1
+    CollatzDict[num] = 1
+    return CollatzDict[num]
   if num%2 == 0:
-    return Collatz(num/2)+1
+    CollatzDict[num] = Collatz(int(num/2))+1
+    return CollatzDict[num]
   elif num%2 == 1:
-    return Collatz(3*num+1)+1
+    CollatzDict[num] = Collatz(3*num+1)+1
+    return CollatzDict[num]
 
 begin = time.time()
+CollatzDict = {}
 maxNum = 1000000
 sequenceNumList = []
 for i in range(1,maxNum):
