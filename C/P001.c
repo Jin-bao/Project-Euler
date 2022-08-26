695aaa9056ecfcb270fe4bf1bdb38972085d03c0
Glass <46128188+Jin-bao@users.noreply.github.com>
2022-08-26 23:52:53 +0800

    Add files via upload

---
+++ C/P001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+  int providedNum = 1000;
+  int mtpSum = 0;
+
+  for (int i=0; i<providedNum; i++){
+    if (i%3 == 0)
+      mtpSum += i;
+    if (i%5 == 0 && i%3 != 0)
+      mtpSum += i;
+  }
+
+  printf("%d", mtpSum);
+}
